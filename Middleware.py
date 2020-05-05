03d6bbfbced0217b3d47452bde32c1beb11faac3
BayMRX <mujian2009@126.com>
2020-05-05 20:16:36 +0800

    修复部分情况下选择数据库或数据表会造成程序崩溃的bug

--- Middleware.py
+++ Middleware.py
@@ -17,8 +17,8 @@ class MyMainForm(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.login_pushButton.clicked.connect(self.openForm)
         self.statusbar.showMessage(' MySQL未连接')  # 主界面底部状态栏
-        self.db_comboBox.activated.connect(self.db_change)
-        self.tb_comboBox.activated.connect(self.tb_change)
+        self.db_comboBox.currentIndexChanged.connect(self.db_change)
+        self.tb_comboBox.currentIndexChanged.connect(self.tb_change)
         self.move2left_pushButton.clicked.connect(self.move2left)
         self.move2right_pushButton.clicked.connect(self.move2right)
 
@@ -43,61 +43,64 @@ class MyMainForm(QMainWindow, Ui_MainWindow):
 
     def db_change(self):  # 数据库下拉菜单选项修改时执行此函数
         self.cur_db = self.db_comboBox.currentText()
-        self.statusbar.showMessage(' MySQL已连接| 当前数据库: ' + self.cur_db)
-        self.db_comboBox.setToolTip(self.cur_db)
-        # print(type(cur_db),cur_db)
-        db.select_db(self.cur_db)
-        # 将当前所选数据库中的所有表添加到数据表下拉菜单中
-        cursor.execute("show full tables where Table_type = 'BASE TABLE' ")
-        item = []
-        for i in cursor.fetchall():
-            item.append(i[0])
-        self.tb_comboBox.clear()
-        self.tb_comboBox.addItems(item)
-        self.left_tableWidget.setRowCount(0)
-        self.right_tableWidget.setRowCount(0)
-        for inx, val in enumerate(item):
-            self.tb_comboBox.setItemData(inx, val, Qt.ToolTipRole)
+        if self.cur_db != '':
+            self.statusbar.showMessage(' MySQL已连接| 当前数据库: ' + self.cur_db)
+            self.db_comboBox.setToolTip(self.cur_db)
+            # print(type(cur_db),cur_db)
+            db.select_db(self.cur_db)
+            # 将当前所选数据库中的所有表添加到数据表下拉菜单中
+            cursor.execute("show full tables where Table_type = 'BASE TABLE' ")
+            item = []
+            for i in cursor.fetchall():
+                item.append(i[0])
+            self.tb_comboBox.clear()
+            self.tb_comboBox.addItems(item)
+            if cur_tb=='':
+                self.left_tableWidget.setRowCount(0)
+                self.right_tableWidget.setRowCount(0)
+            for inx, val in enumerate(item):
+                self.tb_comboBox.setItemData(inx, val, Qt.ToolTipRole)
 
     def tb_change(self):  # 数据表下拉菜单选项修改时执行此函数
         global db, cursor, cur_tb, pk_index, pk_name, v_dict
         cur_tb = self.tb_comboBox.currentText()
-        self.statusbar.showMessage(' MySQL已连接| 当前数据库: ' + self.cur_db + ', 当前数据表: ' + cur_tb)
-        self.tb_comboBox.setToolTip(cur_tb)
-        # 下面是读取数据表中的所有属性并根据注释内容是否为加密列添加到对应的列表中
-        sql = 'show full columns from ' + cur_tb  # 查询当前表下的所有属性
-        cursor.execute(sql)
-        res = cursor.fetchall()  # 获取查询结果
-        db.commit()
-        # 清除列表中现有的数据
-        self.left_tableWidget.setRowCount(0)
-        self.right_tableWidget.setRowCount(0)
-        # 暂时禁用排序，防止向列表中添加时出错
-        self.disableSort()
-        left_row = right_row = 0
-        for val in res:
-            # 若注释为空，则添加到左边列表中
-            if val[8] == '':
-                left_row += 1
-                self.left_tableWidget.setRowCount(left_row)
-                self.left_tableWidget.setItem(left_row - 1, 0, QTableWidgetItem(val[0]))
-                self.left_tableWidget.setItem(left_row - 1, 1, QTableWidgetItem(val[1]))
-                # sql = "ALTER TABLE " + cur_tb + " MODIFY COLUMN " + val[0] + " " + val[1] + " COMMENT '" + val[1] + "'"
-                # cursor.execute(sql)
-            else:
-                # 注释不为空则将注释按逗号分隔成单词列表，若加密列（列表第一个元素标记为en）则添加到右边列表中，否则添加到左边列表中
-                comm = val[8].split(",")
-                if comm[0] == "en":
-                    right_row += 1
-                    self.right_tableWidget.setRowCount(right_row)
-                    self.right_tableWidget.setItem(right_row - 1, 0, QTableWidgetItem(val[0]))
-                    self.right_tableWidget.setItem(right_row - 1, 1, QTableWidgetItem(comm[1]))
-                else:
+        if cur_tb != '':
+            self.statusbar.showMessage(' MySQL已连接| 当前数据库: ' + self.cur_db + ', 当前数据表: ' + cur_tb)
+            self.tb_comboBox.setToolTip(cur_tb)
+            # 下面是读取数据表中的所有属性并根据注释内容是否为加密列添加到对应的列表中
+            sql = 'show full columns from ' + cur_tb  # 查询当前表下的所有属性
+            cursor.execute(sql)
+            res = cursor.fetchall()  # 获取查询结果
+            db.commit()
+            # 清除列表中现有的数据
+            self.left_tableWidget.setRowCount(0)
+            self.right_tableWidget.setRowCount(0)
+            # 暂时禁用排序，防止向列表中添加时出错
+            self.disableSort()
+            left_row = right_row = 0
+            for val in res:
+                # 若注释为空，则添加到左边列表中
+                if val[8] == '':
                     left_row += 1
                     self.left_tableWidget.setRowCount(left_row)
                     self.left_tableWidget.setItem(left_row - 1, 0, QTableWidgetItem(val[0]))
                     self.left_tableWidget.setItem(left_row - 1, 1, QTableWidgetItem(val[1]))
-        self.enableSort()
+                    # sql = "ALTER TABLE " + cur_tb + " MODIFY COLUMN " + val[0] + " " + val[1] + " COMMENT '" + val[1] + "'"
+                    # cursor.execute(sql)
+                else:
+                    # 注释不为空则将注释按逗号分隔成单词列表，若加密列（列表第一个元素标记为en）则添加到右边列表中，否则添加到左边列表中
+                    comm = val[8].split(",")
+                    if comm[0] == "en":
+                        right_row += 1
+                        self.right_tableWidget.setRowCount(right_row)
+                        self.right_tableWidget.setItem(right_row - 1, 0, QTableWidgetItem(val[0]))
+                        self.right_tableWidget.setItem(right_row - 1, 1, QTableWidgetItem(comm[1]))
+                    else:
+                        left_row += 1
+                        self.left_tableWidget.setRowCount(left_row)
+                        self.left_tableWidget.setItem(left_row - 1, 0, QTableWidgetItem(val[0]))
+                        self.left_tableWidget.setItem(left_row - 1, 1, QTableWidgetItem(val[1]))
+            self.enableSort()
 
     def disableSort(self):
         QTableView.setSortingEnabled(self.left_tableWidget, False)
